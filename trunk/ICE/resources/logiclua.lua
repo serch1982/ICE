96f68d7af059ceafd80e342b151bf08e5bf02b92
frank4 <frank4@a7c3e893-904f-42d6-832b-39a831b3ea3e>
2011-04-16 16:02:11 +0000

    lo siento, olvide este fichero

--- trunk/ICE/resources/logiclua.lua
+++ trunk/ICE/resources/logiclua.lua
@@ -1,4 +1,13 @@
-function test(num)
-	s = availableForLua(Vector3(1,2,3))
-	 return s.x * 10
+function EnemyLogicState(enemy,iceLogicLua)
+	--example calling iceEnemy method
+	if (enemy:getState() == iceEnemy.FOLLOWING_TRAJECTORY) then
+		-- example calling iceLogicLua method
+		if(iceLogicLua:enemyIsVisible() == true) then 
+			return iceEnemy.GOING_TO_PLAYER
+		else 
+			return iceEnemy.FOLLOWING_TRAJECTORY
+		end
+	else
+		return  enemy:getState()
+	end
 end
