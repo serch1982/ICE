ea5ce6e28d376c4f48729b9a537246a596a93d8a
sergiia <sergiia@a7c3e893-904f-42d6-832b-39a831b3ea3e>
2011-06-27 12:00:29 +0000

    Me olvidé de subir los últimos mp3 y el .lua

--- ICE/media/scripts/lua/kamikaze.lua
+++ ICE/media/scripts/lua/kamikaze.lua
@@ -2,20 +2,26 @@
 -- param: actual state
 -- return: new state
 function KamikazeLogic( pEnemy )
+-- 	STOPPED
 	if pEnemy:getState() == enemy.STOPPED then
 		if pEnemy:isActive() then
 			pEnemy:setState(enemy.ATTACK)
 		elseif not pEnemy:isAlive() then
 			pEnemy:setState(enemy.INACTIVE)
 		end
+--	ATTACK
 	elseif pEnemy:getState() == enemy.ATTACK then
 		if not pEnemy:isAlive() then
 			pEnemy:setState(enemy.DYING)
 		end
+-- 	DYING
 	elseif pEnemy:getState() == enemy.DYING then
 		if pEnemy:isAnimDyingEnded() then
-			pEnemy:setState(enemy.INACTIVE)
+			pEnemy:setState(enemy.DEAD)
 		end
+-- 	DEAD, After DYING
+	elseif pEnemy:getState() == enemy.DEAD then
+		pEnemy:setState(enemy.INACTIVE)
 	end
 	return pEnemy:getState()
 end
